f2e4add1591a019e8ffdf60c96e0c74f479d3519
Ivan Belonogov <Belonogov95@gmail.com>
2017-05-01 22:52:32 +0200

    Update simplex.cpp

--- musthave/simplex.cpp
+++ musthave/simplex.cpp
@@ -1,14 +1,16 @@
 
-struct Simplex {
-    ll a[MAX_M][MAX_N];
-    ll b[MAX_M];
-    ll c[MAX_N];
-    ll v;    
+namespace simplex {
+    dbl a[MAX_M][MAX_N];
+    dbl b[MAX_M];
+    dbl c[MAX_N];
+    dbl v;    
     ll n, m;
-    ll left[MAX_M];
-    ll up[MAX_N];
+    int left[MAX_M];
+    int up[MAX_N];
+    int pos[MAX_N];
+    dbl res[MAX_N];
 
-    void init(ll nn, ll mm) {
+    void init(int nn, int mm) {
         n = nn;
         m = mm;
         v = 0;
@@ -21,30 +23,27 @@ struct Simplex {
             c[i] = 0;
     }
 
-    int pos[MAX_N];
-
-    void pivot(ll x, ll y) {
+    void pivot(int x, int y) {
         swap(left[x], up[y]); 
-        ll k = a[x][y];
-        assert(abs(k) == 1);
+        dbl k = a[x][y];
         a[x][y] = 1;
         b[x] /= k;
         int cur = 0;
         for (int i = 0; i < n; i++) {
             a[x][i] = a[x][i] / k;
-            if (a[x][i] != 0)
+            if (!eq(a[x][i], 0))
                 pos[cur++] = i;
         }
         
         for (int i = 0; i < m; i++) {
-            if (i == x || a[i][y] == 0) continue;
-            ll cof = a[i][y];
+            if (i == x || eq(a[i][y], 0)) continue;
+            dbl cof = a[i][y];
             b[i] -= cof * b[x];
             a[i][y] = 0;
             for (int j = 0; j < cur; j++)
                 a[i][pos[j]] -= cof * a[x][pos[j]];
         }
-        ll cof = c[y];
+        dbl cof = c[y];
         v += cof * b[x];
         c[y] = 0;
         for (int i = 0; i < cur; i++) {
@@ -52,7 +51,6 @@ struct Simplex {
         }
     }
 
-    ll res[MAX_N];
     void solve() {
         for (int i = 0; i < n; i++)
             up[i] = i;
@@ -62,14 +60,13 @@ struct Simplex {
         while (1) {
             int x = -1;
             for (int i = 0; i < m; i++)
-                if (b[i] < 0 && (x == -1 || b[i] < b[x])) {
+                if (ls(b[i], 0) && (x == -1 || b[i] < b[x])) {
                     x = i;
                 }
             if (x == -1) break;
-
             int y = -1;
             for (int j = 0; j < n; j++)
-                if (a[x][j] < 0) {
+                if (ls(a[x][j], 0)) {
                     y = j; 
                     break;
                 }
@@ -81,14 +78,14 @@ struct Simplex {
         while (1) {
             int y = -1;
             for (int i = 0; i < n; i++)
-                if (c[i] > 0  && (y == -1 || (c[i] > c[y]))) {
+                if (ls(0, c[i])  && (y == -1 || (c[i] > c[y]))) {
                     y = i;
                 }
             if (y == -1) break;
             
             int x = -1;
             for (int i = 0; i < m; i++) {
-                if (a[i][y] > 0) {
+                if (ls(0, a[i][y])) {
                     if (x == -1 || (b[i] / a[i][y] < b[x] / a[x][y])) {
                         x = i;
                     } 
@@ -108,7 +105,9 @@ struct Simplex {
             }
         } 
     }
-     // HOW TO USE ::
+    // HOW TO USE ::
+    // -- call init(n, m)
+    // -- call solve()
     // -- variables in "up" equals to zero 
     // -- variables in "left" equals to b
     // -- max: c * x
@@ -116,8 +115,4 @@ struct Simplex {
     // -- answer in "v"
     // -- sertificate in "res"
 };
-
-   
-
-
 
