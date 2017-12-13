746b99108d8c9db6379f1f390d43e9cb725983d0
izban <izban@mail.ru>
2017-12-13 15:00:39 +0300

    simplex

--- final/numeric/simplex.cpp
+++ final/numeric/simplex.cpp
@@ -1,120 +1,50 @@
-
-namespace simplex {
-    const int MAX_N = -1; // number of variables
-    const int MAX_M = -1; // number of inequalities
-    dbl a[MAX_M][MAX_N];
-    dbl b[MAX_M];
-    dbl c[MAX_N];
-    dbl v;    
-    ll n, m;
-    int left[MAX_M];
-    int up[MAX_N];
-    int pos[MAX_N];
-    dbl res[MAX_N];
-
-    void init(int nn, int mm) {
-        n = nn;
-        m = mm;
-        v = 0;
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                a[i][j] = 0;
-        for (int i = 0; i < m; i++)
-            b[i] = 0;
-        for (int i = 0; i < n; i++)
-            c[i] = 0;
-    }
-
-    void pivot(int x, int y) {
-        swap(left[x], up[y]); 
-        dbl k = a[x][y];
+vector<double> simplex(vector<vector<double> > a) {
+    int n = a.size() - 1;
+    int m = a[0].size() - 1;
+    vector<int> left(n + 1), up(m + 1);
+    iota(up.begin(), up.end(), 0);
+    iota(left.begin(), left.end(), m);
+    auto pivot = [&](int x, int y) {
+        swap(left[x], up[y]);
+        double k = a[x][y];
         a[x][y] = 1;
-        b[x] /= k;
-        int cur = 0;
-        for (int i = 0; i < n; i++) {
-            a[x][i] = a[x][i] / k;
-            if (!eq(a[x][i], 0))
-                pos[cur++] = i;
+        vector<int> vct;
+        for (int j = 0; j <= m; j++) {
+            a[x][j] /= k;
+            if (!eq(a[x][j], 0)) vct.push_back(j);
         }
-        
-        for (int i = 0; i < m; i++) {
-            if (i == x || eq(a[i][y], 0)) continue;
-            dbl cof = a[i][y];
-            b[i] -= cof * b[x];
+        for (int i = 0; i <= n; i++) {
+            if (eq(a[i][y], 0) || i == x) continue;
+            k = a[i][y];
             a[i][y] = 0;
-            for (int j = 0; j < cur; j++)
-                a[i][pos[j]] -= cof * a[x][pos[j]];
+            for (int j : vct) a[i][j] -= k * a[x][j];
         }
-        dbl cof = c[y];
-        v += cof * b[x];
-        c[y] = 0;
-        for (int i = 0; i < cur; i++) {
-            c[pos[i]] -= cof * a[x][pos[i]];
-        }
+    };
+    while (1) {
+        int x = -1;
+        for (int i = 1; i <= n; i++) if (ls(a[i][0], 0) && (x == -1 || a[i][0] < a[x][0])) x = i;
+        if (x == -1) break;
+        int y = -1;
+        for (int j = 1; j <= m; j++) if (ls(a[x][j], 0) && (y == -1 || a[x][j] < a[x][y])) y = j;
+        if (y == -1) assert(0); // infeasible
+        pivot(x, y);
     }
-
-    void solve() {
-        for (int i = 0; i < n; i++)
-            up[i] = i;
-        for (int i = 0; i < m; i++)
-            left[i] = i + n;
-
-        while (1) {
-            int x = -1;
-            for (int i = 0; i < m; i++)
-                if (ls(b[i], 0) && (x == -1 || b[i] < b[x])) {
-                    x = i;
-                }
-            if (x == -1) break;
-            int y = -1;
-            for (int j = 0; j < n; j++)
-                if (ls(a[x][j], 0)) {
-                    y = j; 
-                    break;
-                }
-            if (y == -1) {
-                assert(false); // no solution
-            }
-            pivot(x, y);
-        }
-        while (1) {
-            int y = -1;
-            for (int i = 0; i < n; i++)
-                if (ls(0, c[i])  && (y == -1 || (c[i] > c[y]))) {
-                    y = i;
-                }
-            if (y == -1) break;
-            
-            int x = -1;
-            for (int i = 0; i < m; i++) {
-                if (ls(0, a[i][y])) {
-                    if (x == -1 || (b[i] / a[i][y] < b[x] / a[x][y])) {
-                        x = i;
-                    } 
-                }
-            }
-            if (y == -1) {
-                assert(false); // infinite solution
-            }
-            pivot(x, y);
-        }
-
-        memset(res, 0, sizeof(res));
-
-        for (int i = 0; i < m; i++) {
-            if (left[i] < n) {
-                res[left[i]] = b[i];
-            }
-        } 
+    while (1) {
+        int y = -1;
+        for (int j = 1; j <= m; j++) if (ls(0, a[0][j]) && (y == -1 || a[0][j] > a[0][y])) y = j;
+        if (y == -1) break;
+        int x = -1;
+        for (int i = 1; i <= n; i++) if (ls(0, a[i][y]) && (x == -1 || a[i][0] / a[i][y] < a[x][0] / a[x][y])) x = i;
+        if (x == -1) assert(0); // unbounded
+        pivot(x, y);
     }
-    // HOW TO USE ::
-    // -- call init(n, m)
-    // -- call solve()
-    // -- variables in "up" equals to zero 
-    // -- variables in "left" equals to b
-    // -- max: c * x
-    // -- b[i] >= a[i] * x
-    // -- answer in "v"
-    // -- sertificate in "res"
-};
-
+    vector<double> ans(m + 1);
+    for (int i = 1; i <= n; i++) if (left[i] <= m) ans[left[i]] = a[i][0];
+    ans[0] = -a[0][0];
+    return ans;
+}
+// j=1..m: x[j]>=0
+// i=1..n: sum(j=1..m) A[i][j]*x[j] <= A[i][0]
+// max sum(j=1..m) A[0][j]*x[j]
+// res[0] is answer
+// res[1..m] is certificate
