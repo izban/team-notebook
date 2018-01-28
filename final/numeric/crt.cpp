503dcd88b0794a37840c7391e995408456a08461
Ivan Belonogov <Belonogov95@gmail.com>
2018-01-28 23:44:08 +0300

    Update crt.cpp

--- final/numeric/crt.cpp
+++ final/numeric/crt.cpp
@@ -8,3 +8,11 @@ int CRT(int a1, int m1, int a2, int m2)
 {
     return (a1 - a2 % m1 + m1) * (ll)rev(m2, m1) % m1 * m2 + a2;
 }
+
+ll mul( ll a, ll b, ll m ) { // works for MOD 8e18
+    ll k = (ll)((long double)a * b / m);
+    ll r = a * b - m * k;
+    if (r < 0) r += m;
+    if (r >= m) r -= m;
+    return r;
+}
