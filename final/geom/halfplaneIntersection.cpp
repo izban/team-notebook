2920859af6ee60e5297d5ab37aad8021db19c191
Ivan Belonogov <Belonogov95@gmail.com>
2018-04-10 20:02:39 +0300

    Update halfplaneIntersection.cpp

--- final/geom/halfplaneIntersection.cpp
+++ final/geom/halfplaneIntersection.cpp
@@ -1,73 +1,62 @@
+
 int getPart(pt v) {
-    return less(0, v.y) || (equal(0, v.y) && less(v.x, 0));
+    return ls(v.y, 0) || (eq(0, v.y) && ls(v.x, 0));
 }
 
 int cmpV(pt a, pt b) {
     int partA = getPart(a);
     int partB = getPart(b);
-    if (partA < partB) return -1; 
-    if (partA > partB) return 1;
-    if (equal(0, a * b)) return 0;
+    if (partA < partB) return 1; 
+    if (partA > partB) return -1;
+    if (eq(0, a * b)) return 0;
     if (0 < a * b) return -1;
     return 1;
 }
 
 double planeInt(vector<Line> l) {
-    int n = l.size();
     sort(all(l), [](Line a, Line b) {
             int r = cmpV(a.v, b.v);
             if (r != 0) return r < 0;
-            return a.O % a.v.rotate() < b.O % a.v.rotate();
+            return a.O % a.v.rotate() > b.O % a.v.rotate();
         });
     
-    int cur = 0;
-    for (int i = 0; i < n; ) {
-        int j = i;
-        for (; i < n && cmpV(l[j].v, l[i].v) == 0 && cmpV(l[i].v, l[j].v) == 0; i++);
-        l[cur++] = l[i - 1];
-    }
-    n = cur;
-
-    for (int i = 0; i < n; i++)
+    l.resize(unique(all(l), [](Line A, Line B) { return cmpV(A.v, B.v) == 0; }) - l.begin());
+    for (int i = 0; i < sz(l); i++)
         l[i].id = i;
 
+    // if an infinite answer is possible
     int flagUp = 0;
     int flagDown = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < sz(l); i++) {
         int part = getPart(l[i].v);
         if (part == 1) flagUp = 1;
         if (part == 0) flagDown = 1;
     }
     if (!flagUp || !flagDown) return -1;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < sz(l); i++) {
         pt v = l[i].v;
-        pt u = l[(i + 1) % n].v;
-        if (equal(0, v * u) && less(v % u, 0)) {
+        pt u = l[(i + 1) % sz(l)].v;
+        if (eq(0, v * u) && ls(v % u, 0)) {
             pt dir = l[i].v.rotate();
-            if (lessE(l[(i + 1) % n].O % dir, l[i].O % dir)) return 0;
+            if (le(l[(i + 1) % sz(l)].O % dir, l[i].O % dir)) return 0;
             return -1;
         }
-        if (less(v * u, 0))
+        if (ls(v * u, 0))
             return -1;
     }  
-    
-    cur = 0;
-    vector<Line> st(n * 2);
+    // main part
+    vector<Line> st;
     for (int tt = 0; tt < 2; tt++) {
-        for (int i = 0; i < n; i++) {
-            for (; cur >= 2; cur--) {
-                pt G = st[cur - 1] * l[i];
-                if (!lessE(st[cur - 2].v * (G - st[cur - 2].O), 0))
-                    break;
-            }
-            st[cur++] = l[i];
-            if (cur >= 2 && lessE(st[cur - 2].v * st[cur - 1].v, 0)) return 0;
+        for (auto L: l) {
+            for (; sz(st) >= 2 && le(st[sz(st) - 2].v * (st.back() * L - st[sz(st) - 2].O), 0); st.pop_back());
+            st.pb(L);
+            if (sz(st) >= 2 && le(st[sz(st) - 2].v * st.back().v, 0)) return 0; // useless line
         }
     }
-    vector < int > use(n, -1); 
+    vector<int> use(sz(l), -1); 
     int left = -1, right = -1;
-    for (int i = 0; i < cur; i++) {
+    for (int i = 0; i < sz(st); i++) {
         if (use[st[i].id] == -1) {
             use[st[i].id] = i;
         }
@@ -77,10 +66,10 @@ double planeInt(vector<Line> l) {
             break;
         }
     }
-    vector < Line > tmp;
+    vector<Line> tmp;
     for (int i = left; i < right; i++)
         tmp.pb(st[i]);
-    vector < pt > res;
+    vector<pt> res;
     for (int i = 0; i < (int)tmp.size(); i++) 
         res.pb(tmp[i] * tmp[(i + 1) % tmp.size()]);
     double area = 0;
